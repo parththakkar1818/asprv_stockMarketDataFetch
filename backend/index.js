74ab1370c19b2c512fec09b67f7c11d760fc4f89
parththakkar1818 <121672669+parththakkar1818@users.noreply.github.com>
2024-01-04 12:34:35 +0530

    Update index.js added cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-  origin: "",
+  origin: 'https://asprv-stock-market-prediction-frontend.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
