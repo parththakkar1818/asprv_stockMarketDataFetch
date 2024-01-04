6f15c44b479b6d3e7371cb95082bae3671103a72
parththakkar1818 <121672669+parththakkar1818@users.noreply.github.com>
2024-01-04 12:18:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,12 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "",
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 
 app.get("/", (req, res) => {
   res.send("app is working..");
