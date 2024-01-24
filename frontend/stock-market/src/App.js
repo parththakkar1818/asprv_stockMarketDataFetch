e1d28ec0f3c1a09c015329851022538f795c2894
parththakkar1818 <121672669+parththakkar1818@users.noreply.github.com>
2024-01-24 22:53:50 +0530

    Update App.js second

--- frontend/stock-market/src/App.js
+++ frontend/stock-market/src/App.js
@@ -1,15 +1,26 @@
-import './App.css';
+import "./App.css";
 import "./index.css";
-import Navbar from './components/Navbar';
-// import SearchableSelect from './components/SearchBarSelect';
-import StockDataApp from './components/stock';
-
+import Navbar from "./components/Navbar";
+import StockDataApp from "./components/stock";
+import PredictStockPage from "./components/PredictStockPage";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer
+ from "./components/Footer";
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-      <StockDataApp />
-    </div>
+      <Routes>
+        <Route path="/" element={<StockDataApp />} />
+        <Route
+          // path="/predictstock/:startDate/:endDate/:stockSymbol"
+          path="predict"
+          element={<PredictStockPage />}
+        />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
