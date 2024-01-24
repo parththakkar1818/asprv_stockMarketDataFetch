6d1f74835fe1a98fd45cacdfbc76d85fb8c2c860
parththakkar1818 <121672669+parththakkar1818@users.noreply.github.com>
2024-01-24 23:06:33 +0530

    Update PredictStockPage.jsx adding backend link in place of localhost

--- frontend/stock-market/src/components/PredictStockPage.jsx
+++ frontend/stock-market/src/components/PredictStockPage.jsx
@@ -29,7 +29,7 @@ const PredictStockPage = () => {
   const predictStockPrice = async (startDate , endDate , stockSymbol) => {
     try {
       setLoading(true);
-      const predictionResponse = await fetch(`http://localhost:3001/predictstock/${startDate}/${endDate}/${stockSymbol}`,{
+      const predictionResponse = await fetch(`https://asprv-stock-market-prediction-yb16.vercel.app/predictstock/${startDate}/${endDate}/${stockSymbol}`,{
         method: "POST",
           headers: {
             "Content-Type": "application/json",
